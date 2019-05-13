b5e782889a1a71f3279df78f67ff817a512973fb
Ankit <ankitfriend2000@gmail.com>
2019-05-13 20:21:52 +0530

    adding ui test cases

--- Contact/ContactUITests/ContactUITests.swift
+++ Contact/ContactUITests/ContactUITests.swift
@@ -26,9 +26,63 @@ class ContactUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testFirstScreen() {
+        let app = XCUIApplication()
+        
+        XCTAssertEqual(app.navigationBars.element(boundBy: 0).identifier, "Contact")
     }
-
+    
+    func testAddContactAndCancelScreen() {
+        
+        let app = XCUIApplication()
+        let addButton = app.navigationBars["Contact"].buttons["Add"]
+        XCTAssertTrue(addButton.exists)
+        addButton.tap()
+        
+        let cancelButton = app.buttons["Cancel"]
+        XCTAssertTrue(cancelButton.exists)
+        cancelButton.tap()
+        XCTAssertEqual(app.navigationBars.element(boundBy: 0).identifier, "Contact")
+                ///
+    }
+    
+    func testAddingValidContact() {
+        let firstName = "Ankit"
+        let lastname = "testing"
+        let mobile = "9999999999"
+        let email = "testing@testing.com"
+        
+        let app = XCUIApplication()
+        app.navigationBars["Contact"].buttons["Add"].tap()
+        let tablesQuery = app.tables
+        let table = app.tables.element(boundBy: 0)
+        let firstTextField = table.cells.element(boundBy: 0)
+//        let firstTextField = tablesQuery.tables.cells.containing(.staticText, identifier:"First Name").children(matching: .textField).element
+        expectation(for: NSPredicate(format: "exists == 1"), evaluatedWith: firstTextField, handler: nil)
+        waitForExpectations(timeout: 2, handler: nil)
+        XCTAssertTrue(firstTextField.exists)
+        firstTextField.tap()
+        firstTextField.typeText(firstName)
+        
+        let lastTextField =  tablesQuery.cells.containing(.staticText, identifier:"Last Name").children(matching: .textField).element
+        XCTAssertTrue(lastTextField.exists)
+        lastTextField.tap()
+        lastTextField.typeText(lastname)
+        
+        let mobileTextField =  tablesQuery.cells.containing(.staticText, identifier:"mobile").children(matching: .textField).element
+        XCTAssertTrue(mobileTextField.exists)
+        mobileTextField.tap()
+        mobileTextField.typeText(mobile)
+        
+        let emailTextField =  tablesQuery.cells.containing(.staticText, identifier:"email").children(matching: .textField).element
+        XCTAssertTrue(emailTextField.exists)
+        emailTextField.tap()
+        emailTextField.typeText(email)
+        
+        let doneButton = app.buttons["Done"]
+        XCTAssertTrue(doneButton.exists)
+        doneButton.tap()
+        //
+        
+    }
 }
